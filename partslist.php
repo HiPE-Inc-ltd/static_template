b273e19e921094c4b169dca005565b8f1509b125
CatMeowlet <jamesvincent.ocariza@gmail.com>
2022-01-18 11:36:01 +0800

    change: partial documentation

--- partslist.php
+++ partslist.php
@@ -132,7 +132,7 @@ EXTRA
     </p>
     <div class="l-row l-row--wrap partslist__inner u-mb-30">
         <div class="u-col-l-5  u-col-responsive-12">
-            contruction in progress...... 
+            contruction in progress......
         </div>
         <div class="u-col-l-6  u-col-responsive-12">
             <pre>
@@ -142,6 +142,274 @@ EXTRA
     </div>
     <p class="partslist__heading">
         CARD COMPONENT VARIATION
+    </p>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5 u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('
+PATTERN
+')) ?> 
+            </pre>
+        </div>
+        <div class="u-col-l-6 u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('
+EXTRA 
+')) ?> 
+            </pre>
+        </div>
+    </div>
+    <p class="partslist__heading">
+        FORM COMPONENT
+    </p>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <input type="text" class="c-text01" name="name" id="name" placeholder="Dummy Text">
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<input type="text" class="c-text01" placeholder="Dummy Text">')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <textarea class="c-textarea01" name="message" id="message" cols="30" rows="10" placeholder="Dummy Text"></textarea>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<textarea class="c-textarea01" name="message" id="message" cols="30" rows="10"></textarea>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <select class="c-select01" name="selection_cars" id="selection_cars">
+                <option selected>Please choose..</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<select class="c-select01" name="selection_cars" id="selection_cars">
+            <option selected>Please choose..</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+</select>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <label class="c-checkbox01" for="c-checkbox01">
+                <input class="c-checkbox01__input" type="checkbox" name="xxxx[]" id="c-checkbox01" value="xxxx">
+                <span class="c-checkbox01__parts">
+                    c-checkbox01</span>
+            </label>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<label class="c-checkbox01" for="c-checkbox01">
+                <input class="c-checkbox01__input" type="checkbox" name="xxxx[]" id="c-checkbox01" value="xxxx">
+                <span class="c-checkbox01__parts">
+                    c-checkbox01</span>
+            </label>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <ul class="checkboxGroup">
+                <li class="c-checkbox02">
+                    <input class="c-checkbox02__input" type="checkbox" name="options[]" id="c-checkbox02-01" value="xxxx">
+                    <label class="c-checkbox02__label" for="c-checkbox02-01">
+                        <span class="c-checkbox02__check"></span>
+                        <span class="c-checkbox02__name">
+                            c-checkbox02</span>
+                    </label>
+                </li>
+                <li class="c-checkbox02">
+                    <input class="c-checkbox02__input" type="checkbox" name="options[]" id="c-checkbox02-02" value="xxxx">
+                    <label class="c-checkbox02__label" for="c-checkbox02-02">
+                        <span class="c-checkbox02__check"></span>
+                        <span class="c-checkbox02__name">
+                            c-checkbox02</span>
+                    </label>
+                </li>
+            </ul>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<ul class="checkboxGroup">
+                <li class="c-checkbox02">
+                    <input class="c-checkbox02__input" type="checkbox" name="options[]" id="c-checkbox02-01" value="xxxx">
+                    <label class="c-checkbox02__label" for="c-checkbox02-01">
+                        <span class="c-checkbox02__check"></span>
+                        <span class="c-checkbox02__name">
+                            c-checkbox02</span>
+                    </label>
+                </li>
+                <li class="c-checkbox02">
+                    <input class="c-checkbox02__input" type="checkbox" name="options[]" id="c-checkbox02-02" value="xxxx">
+                    <label class="c-checkbox02__label" for="c-checkbox02-02">
+                        <span class="c-checkbox02__check"></span>
+                        <span class="c-checkbox02__name">
+                            c-checkbox02</span>
+                    </label>
+                </li>
+            </ul>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <ul class="checkboxGroup">
+                <li class="c-checkbox03">
+                    <input class="c-checkbox03__input" type="checkbox" name="options[]" id="c-checkbox03-01" value="xxxx">
+                    <label class="c-checkbox03__label" for="c-checkbox03-01">
+                        <span class="c-checkbox03__check"></span>
+                        <span class="c-checkbox03__name">
+                            c-checkbox03</span>
+                    </label>
+                </li>
+                <li class="c-checkbox03">
+                    <input class="c-checkbox03__input" type="checkbox" name="options[]" id="c-checkbox03-02" value="xxxx">
+                    <label class="c-checkbox03__label" for="c-checkbox03-02">
+                        <span class="c-checkbox03__check"></span>
+                        <span class="c-checkbox03__name">
+                            c-checkbox03</span>
+                    </label>
+                </li>
+            </ul>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<ul class="checkboxGroup">
+    <li class="c-checkbox03">
+        <input class="c-checkbox03__input" type="checkbox" name="options[]" id="c-checkbox03-01" value="xxxx">
+            <label class="c-checkbox03__label" for="c-checkbox03-01">
+                <span class="c-checkbox03__check"></span>
+                <span class="c-checkbox03__name">
+                            option 1</span>
+            </label>
+    </li>
+    <li class="c-checkbox03">
+        <input class="c-checkbox03__input" type="checkbox" name="options[]" id="c-checkbox03-02" value="xxxx">
+            <label class="c-checkbox03__label" for="c-checkbox03-02">
+                <span class="c-checkbox03__check"></span>
+                <span class="c-checkbox03__name">
+                        option 2</span>
+            </label>
+    </li>
+</ul>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <label class="c-checkbox04">
+                <input class="c-checkbox04__input" type="checkbox" name="xxxx[]" value="xxxx"><span class="c-checkbox04__parts">c-checkbox04</span>
+            </label>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<label class="c-checkbox04">
+                <input class="c-checkbox04__input" type="checkbox" name="xxxx[]" value="xxxx"><span class="c-checkbox04__parts">c-checkbox04</span>
+            </label>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <div class="c-radio01">
+                <input class="c-radio01__input" type="radio" id="c-radio01" value="xxxx" name="c-radio">
+                <label class="c-radio01__label" for="c-radio01">
+                    <span class="c-radio01__check"></span>
+                    <span class="c-radio01__name">
+                        c-radio01</span>
+                </label>
+            </div>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<div class="c-radio01">
+                <input class="c-radio01__input" type="radio" id="c-radio01" value="xxxx" name="c-radio">
+                <label class="c-radio01__label" for="c-radio01">
+                    <span class="c-radio01__check"></span>
+                    <span class="c-radio01__name">
+                        c-radio01</span>
+                </label>
+            </div>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <div class="c-radio02">
+                <input class="c-radio02__input" type="radio" id="c-radio02" value="xxxx" name="c-radio">
+                <label class="c-radio02__label" for="c-radio02">
+                    <span class="c-radio02__check"></span>
+                    <span class="c-radio02__name">
+                        c-radio02</span>
+                </label>
+            </div>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<div class="c-radio02">
+                <input class="c-radio02__input" type="radio" id="c-radio02" value="xxxx" name="c-radio">
+                <label class="c-radio02__label" for="c-radio02">
+                    <span class="c-radio02__check"></span>
+                    <span class="c-radio02__name">
+                        c-radio02</span>
+                </label>
+            </div>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <div class="c-radio03">
+                <input class="c-radio03__input" type="radio" id="c-radio03" value="xxxx" name="c-radio">
+                <label class="c-radio03__label" for="c-radio03">
+                    <span class="c-radio03__check"></span>
+                    <span class="c-radio03__name">
+                        c-radio03</span>
+                </label>
+            </div>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<div class="c-radio03">
+                <input class="c-radio03__input" type="radio" id="c-radio03" value="xxxx" name="c-radio">
+                <label class="c-radio03__label" for="c-radio03">
+                    <span class="c-radio03__check"></span>
+                    <span class="c-radio03__name">
+                        c-radio03</span>
+                </label>
+            </div>')) ?>
+            </pre>
+        </div>
+    </div>
+    <div class="l-row l-row--wrap partslist__inner u-mb-30">
+        <div class="u-col-l-5  u-col-responsive-12">
+            <label class="c-radio04">
+                <input class="c-radio04__input" type="radio" value="xxxx" name="c-radio">
+                <span class="c-radio04__parts">c-radio04</span>
+            </label>
+        </div>
+        <div class="u-col-l-6  u-col-responsive-12">
+            <pre>
+<?= trim(htmlspecialchars('<label class="c-radio04">
+                <input class="c-radio04__input" type="radio" value="xxxx" name="c-radio">
+                <span class="c-radio04__parts">c-radio04</span>
+            </label>')) ?>
+            </pre>
+        </div>
+    </div>
+    <p class="partslist__heading">
+        FORM COMPONENT VARIATION
     </p>
     <div class="l-row l-row--wrap partslist__inner u-mb-30">
         <div class="u-col-l-5 u-col-responsive-12">
